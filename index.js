0549739a48782c7887da47744ce2dc969f12d37b
MD Biplop Hossain <developerbiplop@gmail.com>
2022-05-19 11:03:51 +0600

    test

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 // mongo db
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.suhti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
